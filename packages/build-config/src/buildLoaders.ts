08a16fe3cafe1f6856137e2ef878736e96decae3
darya <lukina-dasha@mail.ru>
2024-08-09 15:47:32 +0300

    package build-config

---
+++ packages/build-config/src/buildLoaders.ts
@@ -0,0 +1,91 @@
+import { ModuleOptions } from 'webpack';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import ReactRefreshTypeScript from 'react-refresh-typescript';
+import { BuildOptions } from './types/types';
+import { buildBabelLoader } from './babel/buildBabelLoader';
+
+export function buildLoaders(options: BuildOptions): ModuleOptions['rules'] {
+  const isDev = options.mode === 'development';
+
+  const assetLoader = {
+    test: /\.(png|jpg|jpeg|gif)$/i,
+    type: 'asset/resource',
+  };
+
+  const svgrLoaders = {
+    test: /\.svg$/,
+    use: [
+      {
+        loader: '@svgr/webpack',
+        options: {
+          icon: true,
+          svgoConfig: {
+            plugins: [
+              {
+                name: 'convertColors',
+                params: {
+                  currentColor: true,
+                },
+              },
+            ],
+          },
+        },
+      },
+    ],
+  };
+
+  const cssLoaderWithModules = {
+    loader: 'css-loader',
+    options: {
+      modules: {
+        localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]',
+      },
+    },
+  };
+
+  const scssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+      cssLoaderWithModules,
+      'sass-loader',
+    ],
+  };
+
+  // const tsLoader = {
+  //   // Ts-loader умеет работать с JSX
+  //   // если бы мы не работали с ts -- нужен был бы babel-loader
+  //   test: /\.tsx?$/,
+  //   use: 'ts-loader',
+  //   exclude: /node_modules/,
+  // };
+
+  // ускоряется сборка - отключаем проверку типов во время сборки
+  const tsLoader = {
+    test: /\.tsx?$/,
+    exclude: /node_modules/,
+    use: [
+      {
+        loader: 'ts-loader',
+        options: {
+          transpileOnly: isDev,
+          getCustomTransformers: () => ({
+            before: [isDev && ReactRefreshTypeScript()].filter(Boolean),
+          }),
+        },
+      },
+    ],
+  };
+
+  // также есть swc loader, esbuild-loader
+  // const babelLoader = buildBabelLoader(isDev);
+
+  return [
+    assetLoader,
+    svgrLoaders,
+    // порядок важен
+    scssLoader,
+    tsLoader,
+    // babelLoader,
+  ];
+}
---
+++ packages/build-config/src/buildPlugins.ts
@@ -0,0 +1,59 @@
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import { Configuration, DefinePlugin } from 'webpack';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
+import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import ReactRefreshTypeScript from '@pmmmwh/react-refresh-webpack-plugin';
+import CopyPlugin from 'copy-webpack-plugin';
+import path from 'path';
+import { BuildOptions } from './types/types';
+
+export function buildPlugins(options: BuildOptions): Configuration['plugins'] {
+  const {
+    paths, mode, analyzer, platform,
+  } = options;
+  const isProd = mode === 'production';
+  const isDev = mode === 'development';
+
+  const plugins: Configuration['plugins'] = [
+    new HtmlWebpackPlugin({ template: paths.html, favicon: path.resolve(paths.public, 'favicon.ico') }),
+    // new webpack.ProgressPlugin(), прогресс сборки
+    new DefinePlugin({
+      __PLATFORM__: JSON.stringify(platform),
+    }),
+  ];
+
+  if (isProd) {
+    plugins.push(
+      new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash:8].css',
+        chunkFilename: 'css/[name].[contenthash:8].css', // ?
+      }),
+    );
+    // добавляем в build файлы из public
+    plugins.push(
+      new CopyPlugin({
+        patterns: [
+          { from: path.resolve(paths.public, 'locales'), to: path.resolve(paths.output, 'locales') },
+        ],
+      }),
+    );
+  }
+
+  if (isDev) {
+    // plugins.push(
+    //   // выносит проверку типов в отдельный процесс, не нагружая сборку
+    //   new ForkTsCheckerWebpackPlugin(),
+    // );
+    plugins.push(
+      // позволяет подгружать изменения без перезагрузки -- Hot module replacement (hmr)
+      new ReactRefreshTypeScript(),
+    );
+  }
+
+  if (analyzer) {
+    plugins.push(new BundleAnalyzerPlugin());
+  }
+
+  return plugins;
+}
